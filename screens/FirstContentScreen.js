3f6680eebc68af1ad5dc827a99cb528b43a6b0d4
Cecily <cecily.downs@gmail.com>
2018-01-28 09:57:12 -0800

    Add styling for content page

--- screens/FirstContentScreen.js
+++ screens/FirstContentScreen.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, ScrollView, StyleSheet } from 'react-native';
+import BreeSerif from '../components/BreeSerif'
 
 const FirstContentScreen = () => (
-    <Text>Another page</Text>
+    <ScrollView style={styles.container}>
+        <Text style={styles.contentHeading}>
+            <BreeSerif>Page one heading</BreeSerif>
+        </Text>
+        <Text style={styles.contentPageBody}>
+        Some body text
+        </Text>
+    </ScrollView>
 )
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 15
+    },
+    contentPageBody: {
+      fontSize: 16
+    },
+    contentHeading: {
+      fontSize: 25,
+      lineHeight: 24,
+      marginBottom: 10,
+      color: '#f95c13',
+    }
+  });
 
 export default FirstContentScreen
